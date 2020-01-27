24bcc4d24412b1b6e7926b3eb46d6a53983fede9
dql521 <douqianli@qq.com>
2020-01-27 22:21:44 +0800

    按钮组件部分功能完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="row">
+      <do-button>按钮</do-button>
+      <do-button type="primary">按钮</do-button>
+      <do-button type="success">按钮</do-button>
+      <do-button type="info">按钮</do-button>
+      <do-button type="warning">按钮</do-button>
+      <do-button type="danger">按钮</do-button>
+    </div>
+    <div class="row">
+      <do-button plain>按钮</do-button>
+      <do-button plain type="primary">按钮</do-button>
+      <do-button plain type="success">按钮</do-button>
+      <do-button plain type="info">按钮</do-button>
+      <do-button plain type="warning">按钮</do-button>
+      <do-button plain type="danger">按钮</do-button>
+    </div>
+    <div class="row">
+      <do-button round>按钮</do-button>
+      <do-button round type="primary">按钮</do-button>
+      <do-button round type="success">按钮</do-button>
+      <do-button round type="info">按钮</do-button>
+      <do-button round type="warning">按钮</do-button>
+      <do-button round type="danger">按钮</do-button>
+    </div>
+    <div class="row">
+      <do-button icon="do-icon-check" circle></do-button>
+      <do-button icon="do-icon-check" circle type="primary"></do-button>
+      <do-button icon="do-icon-check" circle type="success"></do-button>
+      <do-button icon="do-icon-check" circle type="info"></do-button>
+      <do-button icon="do-icon-check" circle type="warning"></do-button>
+      <do-button icon="do-icon-check" circle type="danger"></do-button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  .row{
+    margin-bottom: 20px;
+    .do-button{
+      margin-right: 20px;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import DoButton from './components/Button.vue'
+import './assets/fonts/font.scss'
 
 Vue.config.productionTip = false
+Vue.component(DoButton.name, DoButton)
 
 new Vue({
   render: h => h(App)
